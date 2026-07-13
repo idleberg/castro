80ba1b793394d4f398c64863cff712aab6e9ab4b
Jan T. Sott <git@idleberg.com>
2026-07-13 19:47:54 +0200

    refactor: update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,13 @@
 import type { APIRoute } from 'astro';
 
-const getRobotsTxt = (sitemapURL?: URL) => `\
+const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
-${sitemapURL ? `\nSitemap: ${sitemapURL.href}` : ''}
+
+Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-	if (!site) {
-		return new Response(getRobotsTxt());
-	}
-
 	const sitemapURL = new URL('sitemap-index.xml', site);
 
 	return new Response(getRobotsTxt(sitemapURL));
